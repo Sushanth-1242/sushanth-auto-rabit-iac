e6e8e1958b719a08cc2212f5b6c6df1795b35fc6
Sushanth <143963166+Sushanth-1242@users.noreply.github.com>
2025-08-07 01:27:50 +0530

    Create main.tf

---
+++ modules/EC2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "my_instance" {
+  ami                      = var.ami_id
+  instance_type            = var.instance_type
+  key_name                 = var.key_name
+  vpc_security_group_ids   = var.security_group_ids
+  subnet_id                = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  # Define the root EBS volume size
+  root_block_device {
+    volume_size = var.ebs_size  # Reference the variable for desired size
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+}
