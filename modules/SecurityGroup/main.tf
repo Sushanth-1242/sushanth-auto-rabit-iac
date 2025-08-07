a9dd13979ac2184691398df683d9b08703b348e6
Sushanth <143963166+Sushanth-1242@users.noreply.github.com>
2025-08-07 17:26:25 +0530

    Update main.tf

--- modules/SecurityGroup/main.tf
+++ modules/SecurityGroup/main.tf
@@ -1,22 +1,24 @@
 resource "aws_security_group" "sg" {
-  count       = length(var.sg_name_prefix)
-  name        = "${var.sg_name_prefix[count.index]}-sg"
-  vpc_id      = var.vpc_id
-  description = "Security group for ${var.sg_name_prefix[count.index]} instances"
+  for_each = { for idx, rule in var.egress_rules : idx => rule }
 
-  dynamic "ingress" {
-    for_each = var.ingress_rules[count.index]
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
+  name_prefix = "${var.sg_name_prefix[each.key]}-sg"
+  description = "Security group for ${var.sg_name_prefix[each.key]}"
+  vpc_id      = var.vpc_id
+
+  # HTTP access from anywhere
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Optional: Using the egress rules defined in the variable
   dynamic "egress" {
-    for_each = var.egress_rules[count.index]
+    for_each = each.value
     content {
+      description = "All outbound traffic"
       from_port   = egress.value.from_port
       to_port     = egress.value.to_port
       protocol    = egress.value.protocol
@@ -24,11 +26,11 @@ resource "aws_security_group" "sg" {
     }
   }
 
-  tags = var.tags
-}
+  tags = merge(var.tags, {
+    Name = "${var.sg_name_prefix[each.key]}-sg"
+  })
 
-
-output "security_group_ids" {
-  description = "List of security group IDs"
-  value       = aws_security_group.sg[*].id
+  lifecycle {
+    create_before_destroy = true
+  }
 }
