9530cf64e3147f379b1fd479f9ebca9e76dce4d1
Sushanth <143963166+Sushanth-1242@users.noreply.github.com>
2025-08-07 15:10:05 +0530

    Update output.tf

--- modules/VPC/output.tf
+++ modules/VPC/output.tf
@@ -13,7 +13,7 @@ output "public_subnet_2_id" {
   value       = aws_subnet.public_subnet_02.id
 }
 
-output "private_subnet_2_id" {
+output "private_subnet_1_id" {
   description = "The ID of the private subnet 2"
   value       = aws_subnet.private_subnet_1.id
 }
