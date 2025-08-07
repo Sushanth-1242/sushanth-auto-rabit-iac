173b688da6c1a28ff311079c29f645d3bf9a86bc
Sushanth <143963166+Sushanth-1242@users.noreply.github.com>
2025-08-07 17:27:24 +0530

    Update variables.tf

--- modules/SecurityGroup/variables.tf
+++ modules/SecurityGroup/variables.tf
@@ -19,14 +19,25 @@ variable "ingress_rules" {
 }
 
 variable "egress_rules" {
-  description = "List of egress rules for each security group"
+  description = "List of egress rules for security groups"
   type = list(list(object({
     from_port   = number
     to_port     = number
     protocol    = string
     cidr_blocks = list(string)
   })))
+  default = [
+    [
+      {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    ]
+  ]
 }
+
 
 variable "tags" {
   description = "Tags to apply to security groups"
