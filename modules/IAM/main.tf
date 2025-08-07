9c3dfcbf6bbfd44e35567fcce58d06b2bd6b62db
Sushanth <143963166+Sushanth-1242@users.noreply.github.com>
2025-08-07 20:54:06 +0530

    Create main.tf

---
+++ modules/IAM/main.tf
@@ -0,0 +1,39 @@
+# modules/IAM/main.tf
+
+resource "aws_iam_role" "code_deploy_role" {
+  name               = "ec2-code-deploy-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach policies for EC2 instances to interact with CodeDeploy, ECR, and other services
+resource "aws_iam_role_policy_attachment" "code_deploy_policy" {
+  role       = aws_iam_role.code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_policy" {
+  role       = aws_iam_role.code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECRReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_read_policy" {
+  role       = aws_iam_role.code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnly"  # Optional if you're using S3
+}
+
+# Create IAM Instance Profile
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "ec2-code-deploy-instance-profile"
+  role = aws_iam_role.code_deploy_role.name
+}
